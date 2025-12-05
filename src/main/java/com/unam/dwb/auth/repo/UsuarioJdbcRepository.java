88f0d815956b3cd8e7959577f5c742bfd1ff83bf
Daniiel314 <36395002+Daniiel314@users.noreply.github.com>
2025-12-05 06:53:39 -0600

    Initial microservice version

---
+++ src/main/java/com/unam/dwb/auth/repo/UsuarioJdbcRepository.java
@@ -0,0 +1,58 @@
+package com.unam.dwb.auth.repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.unam.dwb.auth.domain.Usuario;
+
+@Repository
+public class UsuarioJdbcRepository {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	public Optional<Usuario> findByUsername(String username) {
+		String sql = "SELECT username, correo FROM dwb.usuario WHERE username = ?";
+		
+		List<Usuario> resultadoQuery = jdbcTemplate.query(sql,
+				(rs, row) -> new Usuario(
+						rs.getString("username"),
+						rs.getString("correo")
+						),
+				username);
+		
+		return resultadoQuery.stream().findFirst();
+	}
+	
+	public Optional<Usuario> findByUsernameCredential(String username, String hashPass) {
+		String sql = "SELECT username, correo FROM dwb.usuario WHERE username = ? AND password = ?";
+		
+		List<Usuario> resultadoQuery = jdbcTemplate.query(sql,
+				(rs, row) -> new Usuario(
+						rs.getString("username"),
+						rs.getString("correo")
+						),
+				username, hashPass);
+		
+		return resultadoQuery.stream().findFirst();
+	}
+	
+	public Optional<Usuario> findByCorreoCredential(String correo, String hashPass) {
+		String sql = "SELECT username, correo FROM dwb.usuario WHERE correo = ? AND password = ?";
+		
+		List<Usuario> resultadoQuery = jdbcTemplate.query(sql,
+				(rs, row) -> new Usuario(
+						rs.getString("username"),
+						rs.getString("correo")
+						),
+				correo, hashPass);
+		
+		return resultadoQuery.stream().findFirst();
+	}
+
+}
